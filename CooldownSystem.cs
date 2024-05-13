31756365a0ee3e0b9237e1ca80c0decbf3507908
Philip T <philiptroeger2003@gmail.com>
2024-05-14 00:06:26 +0200

    Implemented the removal of the dial cooldown

---
+++ CooldownSystem.cs
@@ -0,0 +1,21 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace InstaSundialMoondial;
+
+public class CooldownSystem : ModSystem
+{
+    public override void PostUpdateEverything()
+    {
+        base.PostUpdateEverything();
+
+        var dialActive = Main.fastForwardTimeToDawn || Main.fastForwardTimeToDusk;
+        if (dialActive)
+        {
+            return;
+        }
+
+        Main.sundialCooldown = 0;
+        Main.moondialCooldown = 0;
+    }
+}
